3300983473c7095a8caf32f944cd3bf7f2a27b05
zhuzedan <1031155817@qq.com>
2024-03-20 17:11:03 +0800

    fix:打包空白页和地址去#号

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const {defineConfig} = require('@vue/cli-service')
 const path = require("path");
 module.exports = defineConfig({
+    publicPath:'./',
     pluginOptions: {
         "style-resources-loader": {
             preProcessor: "less",
